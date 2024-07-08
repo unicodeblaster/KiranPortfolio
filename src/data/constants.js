261fa337670082fa791e40490e58eaf2e607f431
unknown <knikam2803@gmail.com>
2024-07-08 23:56:35 +0530

    task

---
+++ src/data/constants.js
@@ -0,0 +1,345 @@
+export const Bio = {
+  name: "Kiran Nikam",
+  roles: [
+    "Front End Developer",
+    "Full Stack Developer", 
+    "UI/UX Designer",
+    "Finacial Advisor"
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/unicodeblaster",
+  resume:
+    "",
+  linkedin: "https://www.linkedin.com/in/kiran-nikam-49873422a/",
+  twitter: "",
+  insta: "https://www.instagram.com/?hl=en",
+  facebook: "https://www.facebook.com/profile.php?id=100072258293342",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:"https://tse1.mm.bing.net/th?id=OIP.IyzEkoSpyLZfovs2RR5lYwHaE8&pid=Api&P=0&h=180"
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image:"https://tse1.mm.bing.net/th?id=OIP.aCK3HUSs4N8YGw_9x7vWQAAAAA&pid=Api&P=0&h=180"
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://tse1.mm.bing.net/th?id=OIP.GdUWbrVtbNezIRl6jg_ymAHaEh&pid=Api&P=0&h=180",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:"https://tse4.mm.bing.net/th?id=OIP.1fZjQpkRMKTBGN_7H5YnFwHaGL&pid=Api&P=0&h=180"
+      },
+      {
+        name: "Java",
+        image:"https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg"
+      },
+    
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+     
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+    
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:"https://tse3.mm.bing.net/th?id=OIP.Xa0BEkwl0Zx4qnY9lMbD7gHaHa&pid=Api&P=0&h=180"
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+     
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      { name:"Aws",
+        image:
+        "https://tse1.mm.bing.net/th?id=OIP.ASkbzV1btB5QPvuK4B4F0gHaF4&pid=Api&P=0&h=180",
+
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:"https://tse2.mm.bing.net/th?id=OIP.ThWdBgvFZdoLzqAzgtkSfQHaHa&pid=Api&P=0&h=180"
+      },
+ 
+      {
+        name: "Figma",
+        image: "https://tse4.mm.bing.net/th?id=OIP.rAD6e2doKGrUQoPkWV3QfgHaHa&pid=Api&P=0&h=180"
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://xscientist.in/assets/logo-DiIfhAJv.svg",
+    role: "Front End Developer Intern",
+    company: "xScientist Tech LLP",
+    date: "Sep 2023 - May 2024",
+    desc: "Working on Frontend techanologies,Manages the databases and Client Requirement.",
+    skills: [
+      "Html",
+      "CSS",
+      "AWS",
+      "JavaScript",
+      "React Js",
+      "Redux",
+      "Tailwind Css",
+      "Bootstrap",
+      "Node Js",
+      " Next Js",
+      "Figma"
+    ],
+  doc:" https://drive.google.com/file/d/1oI475A1WXdM_M4_oPKFaFn1FP9xw9Ie0/view?usp=sharing",
+  },
+  
+  {
+    id: 0,
+    img: "https://tse3.mm.bing.net/th?id=OIP.aAqETQ_U0YzEEg-qH4VYXwHaDn&pid=Api&P=0&h=180",
+    role: "WEB Dveloper Intern ", 
+    company: "Invontream Pvt Ltd -Bolt Iot",
+    date: "Mar 2023 - May 2023",
+    desc: "Working on the frontend of the web application using Html,CSS ,JavaScript ,ReactJS.",
+    skills: [
+      "ReactJS",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Bootstrap"
+    ],
+    doc:"https://drive.google.com/file/d/1WZirgWPquiOpD8Af7I_qXFPflwRxSJn9/view?usp=sharing" ,
+  },
+ 
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://tse2.mm.bing.net/th?id=OIP.wrrWd8XTF3kGK8tCboeY6AHaHa&pid=Api&P=0&h=180",
+    school: " CSMSS Chh.Shahu College OF Engineering  Aurangabad",
+    date: "Jun 2020 - Jun 2024",
+    grade: "8.09  CGPA",
+    desc: "I am currently completed a Bachelor's degree in Computer Science and Engineering at CSMSS Chh.Shahu College OF Engineering  Aurangabad . I have completed 8 semesters and have a CGPA of 8.09. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://tse4.mm.bing.net/th?id=OIP.2CeTouvC98xMGMGisagH_wHaHP&pid=Api&P=0&h=180",
+    school: "Vivekanand Arts, Sardar Dalipsingh Commerce & Science College, Aurangabad.",
+    date: "Apr 2019 - Apr 2020",
+ 
+    desc: "I completed my class 12 high school education at This School, where I studied Science.",
+    degree: "(XII), Science ",
+  },
+  {
+    id: 2,
+    img: "https://tse3.mm.bing.net/th?id=OIP.r8WKGkAJ1jBJBiyvgSei5AAAAA&pid=Api&P=0&h=180",
+    school: "Sant Eknath Vidhyalya Chitegoan",
+    date: "Apr 2017 - Apr 2018",
+    grade: "77.3%",
+    desc: "I completed my class 10 education at This School, where I studied Science  and General Studies.",
+    degree: "(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "MH20-Collections",
+  
+    description:
+      "This innovative ecommerce web app offers a seamless shopping experience, allowing you to discover and acquire the finest items across various categories",
+    image:
+      "https://mh-two.vercel.app/img/hero1.png",
+    tags: [
+      "React",
+      "JavaScript",
+      "Firebase",
+      "CSS",
+      "Html",
+      "VS-Code",
+      "Tailwind Framewoek",
+      "Redux",
+    ],
+    category: "web app",
+    github: "https://github.com/unicodeblaster/mh",
+    webapp: "https://mh-two.vercel.app/",
+  },
+  {
+    id: 0,
+    title: "Podstream - InProgress",
+    
+  
+    description:
+      "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+    image:
+      "",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    category: "web app",
+    github: "",
+    webapp: "",
+    member: [
+      {
+        name: "Kiran Nikam",
+        linkedin: "https://www.linkedin.com/in/kiran-nikam-49873422a/",
+        github: "https://github.com/unicodeblaster",
+      },
+      {
+        name: "Rahul Thate",
+        img: "https://avatars.githbusercontent.com/u/100614635?v=4",
+  
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Vexa",
+    
+    description:
+      "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "NodeMailer",
+    ],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  
+  {
+    id: 3,
+    title: "Food Donation App",
+  
+    description:
+   "  In highly populated countries like India, food wastage is a disturbing issue. The streets, garbage bins and landfills have ample proof to prove it. Marriages, canteens, restaurants, social and family get-togethers and functions expel out so much food. In highly populated countries like India, food wastage is a disturbing issue. The streets, garbage bins and landfills have ample proof to prove it. Marriages, canteens, restaurants, social and family get-togethers and functions expel out so much food. Food wastage is not only an indication of hunger or pollution, but also of many economic problems. The high standard of living has resulted in the wastage of food, clothes, etc. because of quick changes in habits and lifestyle. Instead of wasting these things we can put them in use by donating them to various organizations such as orphanages, old age homes, etc. The product is an internet-based android application that basically aims at charity through donations.",
+    image:
+      "https://user-images.githubusercontent.com/54005333/147682382-a0a2671d-6160-44d5-a4fb-dab764103ca4.png",
+    tags: ["Java", "Android studio"],
+    category: "android app",
+    github: "https://github.com/unicodeblaster/AAHAR-APP",
+    
+  },
+
+  {
+    id: 4,
+    title: "Resume Builder",
+   
+    description:"Our project utilizes a template-based approach, allowing users to choose from a variety of professionally-designed resume templates to suit their individual preferences and industry standards. The tool also offers customization options, such as font styles, colors, and layout adjustments, to help users create a unique and visually appealing resume.",
+    image:
+      "https://private-user-images.githubusercontent.com/103228257/346315604-f5db95a2-2e5a-4b69-a5f4-b1495306a65f.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjA0MzA5NTAsIm5iZiI6MTcyMDQzMDY1MCwicGF0aCI6Ii8xMDMyMjgyNTcvMzQ2MzE1NjA0LWY1ZGI5NWEyLTJlNWEtNGI2OS1hNWY0LWIxNDk1MzA2YTY1Zi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNzA4JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDcwOFQwOTI0MTBaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT05NmIwZDZjZWQzMmY3NGY4OWUyOWNkMDkzODcwYWIyMDAzMGRmYmY2NWRjNjc1NGY1NDVhNzEwZDQzNTIxMDBmJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.TvyVdMFSJI9IDdxsFDbkgHf8LZn7JNxID0zg2STvdDo",
+    tags: ["Html", "CSS", "JavaScript", "MySQL","Php"],
+    category: "web app",
+    github: "https://github.com/unicodeblaster/resume-build",
+    webapp: "https://autoresumegenerator.netlify.app/",
+  },
+ 
+  {
+    id: 6,
+    title: "Ganna App",
+   
+    description:
+      "Ganna is a web app that allows users to discover and listen to music from a wide range of genres and artists. Users can create personalized playlists, follow their favorite artists, and explore curated playlists and recommendations based on their listening habits.",
+    image:
+      "https://drive.google.com/file/d/1xMmKEnUG_noOJYCy1sebVf4Bwj5LUxNW/view?usp=drive_link",
+    tags: ["React Js", "API", "JavaScript", "Html","Bootstrap"],
+    category: "web app",
+    github: "https://github.com/unicodeblaster/Simple-Ganna-App",
+    webapp: "https://ganna-webapp.netlify.app",
+  },
+ 
+ 
+];
+
+
