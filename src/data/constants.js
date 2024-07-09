5d9578c89b36bbd2f947e4cb0e368781c85e13ef
Kiran Dnyaneshwar Nikam <103228257+unicodeblaster@users.noreply.github.com>
2024-07-09 12:07:44 +0530

    Update constants.js
    Resume link add

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/unicodeblaster",
   resume:
-    "",
+    "https://drive.google.com/file/d/1mn5wYw3Oz216WmLuT95hQ6RQz_7vrSPY/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/kiran-nikam-49873422a/",
   twitter: "",
   insta: "https://www.instagram.com/?hl=en",
